cd4f00ea0e67fa49cac9e81d3bb8744a1fa08860
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-03-10 12:57:11 +0100

    Add placeholder replacement utility

--- utility.php
+++ utility.php
@@ -4,6 +4,49 @@
 defined( 'ABSPATH' ) OR exit;
 
 // =======================================================================================
+// GENERAL
+// =======================================================================================
+
+/**
+ * Replace (conditional) placeholders in a string with corresponding values
+ *
+ * @since 0.1.0
+ *
+ * @param string $string  The string containing the placeholders.
+ * @param array  $extra   Additional replacements.
+ *
+ * @return string The string with replaced placeholders.
+ */
+
+function fcncn_replace_placeholders( $string, $extra = [] ) {
+  // Setup
+  $replacements = array_merge( [], $extra );
+
+  // Replace conditional placeholders {{#placeholder}}content{{/placeholder}}
+  $string = preg_replace_callback( '/{{\#([\w\d_-]+)}}((?:(?!{{\/\1}}).)*){{\/\1}}/s', function( $matches ) use ( $replacements ) {
+    $placeholder = $matches[1];
+    $replacement = isset( $replacements[ "{{{$placeholder}}}" ] ) ? $replacements[ "{{{$placeholder}}}" ] : '';
+
+    // If the replacement value is not empty, replace the placeholder with the inner content; otherwise, remove the placeholder
+    return ! empty( $replacement ) ? str_replace( $matches[0], $matches[2], $matches[0] ) : '';
+  }, $string );
+
+  // Replace inverted conditional placeholders {{^placeholder}}content{{/placeholder}}
+  $string = preg_replace_callback( '/{{\^([\w\d_-]+)}}((?:(?!{{\/\1}}).)*){{\/\1}}/s', function( $matches ) use ( $replacements ) {
+    $placeholder = $matches[1];
+    $replacement = isset( $replacements[ "{{{$placeholder}}}" ] ) ? $replacements[ "{{{$placeholder}}}" ] : '';
+
+    // If the replacement value is empty, replace the placeholder with the inner content; otherwise, remove the placeholder
+    return empty( $replacement ) ? str_replace( $matches[0], $matches[2], $matches[0] ) : '';
+  }, $string );
+
+  // Replace regular placeholders {placeholder}
+  $string = str_replace( array_keys( $replacements ), array_values( $replacements ), $string );
+
+  return $string;
+}
+
+// =======================================================================================
 // SUBSCRIBERS
 // =======================================================================================
 
