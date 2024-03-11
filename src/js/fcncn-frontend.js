ca258c834af795915d59ec61655341877b314dc1
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-03-11 01:11:51 +0100

    Load modal content via AJAX
    This will allow dynamic content on cached pages.

--- src/js/fcncn-frontend.js
+++ src/js/fcncn-frontend.js
@@ -1,6 +1,18 @@
 const fcncn_modal = document.getElementById('fcncn-subscription-modal');
 
 if (fcncn_modal) {
+  document.querySelector('[data-click-action*="fcncn-load-modal-form"]').addEventListener('click', () => {
+    fcncn_getModalForm();
+  }, { once: true });
+}
+
+/**
+ * Add EventListeners.
+ *
+ * @since 0.1.0
+ */
+
+function fcncn_addEventListeners() {
   // Toggle auth mode
   document.querySelector('[data-click-action="auth-mode"]').addEventListener('click', () => {
     fcncn_modal.querySelector('[data-target="auth-mode"]').hidden = false;
@@ -11,5 +23,29 @@ if (fcncn_modal) {
   document.querySelector('[data-click-action="submit-mode"]').addEventListener('click', () => {
     fcncn_modal.querySelector('[data-target="auth-mode"]').hidden = true;
     fcncn_modal.querySelector('[data-target="submit-mode"]').hidden = false;
+  });
+}
+
+/**
+ * AJAX: Get the modal form.
+ *
+ * @since 0.1.0
+ */
+
+function fcncn_getModalForm() {
+  // Setup
+  const payload = {
+    'action': 'fcncn_ajax_get_form_content'
+  };
+
+  // Request
+  fcn_ajaxPost(payload)
+  .then(response => {
+    if (response.success) {
+      fcncn_modal.querySelector('[data-target="fcncn-modal-loader"]').innerHTML = response.data.html;
+    }
+  })
+  .then(() => {
+    fcncn_addEventListeners();
   });
 }
