9fead4e8445e348a6979f9123b6b312ddd237637
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-03-14 20:10:31 +0100

    Add description to notification email settings

--- admin.php
+++ admin.php
@@ -730,7 +730,7 @@ function fcnen_settings_page() {
           </div>
 
           <div class="fcnen-box__row fcnen-box__vertical fcnen-template-wrapper hidden" id="layout-confirmation">
-            <div><?php _e( 'This email is sent when a new subscription is submitted, prompting the subscriber to confirm it as security measure against fraudulent submissions. Anyone could enter anyone’s email address, after all. If not confirmed within 24 hours, the subscription and all data will be deleted once the cron job runs (every 12 hours). Make sure to always include the necessary replacement tokens.', 'fcnen' ); ?></div>
+            <div class="fcnen-replacement-tokens"><?php _e( 'This email is sent when a new subscription is submitted, prompting the subscriber to confirm it as security measure against fraudulent submissions. Anyone could enter anyone’s email address, after all. If not confirmed within 24 hours, the subscription and all data will be deleted once the cron job runs (every 12 hours). Make sure to always include the necessary replacement tokens.', 'fcnen' ); ?></div>
             <div class="fcnen-spacer"></div>
             <div class="fcnen-left-right-wrap">
               <label for="fcnen-template-subject-confirmation" class="offset-top"><?php _e( 'Subject', 'fcnen' ); ?></label>
@@ -758,7 +758,7 @@ function fcnen_settings_page() {
           </div>
 
           <div class="fcnen-box__row fcnen-box__vertical fcnen-template-wrapper hidden" id="layout-code">
-            <div><?php _e( 'This email can be manually triggered in the subscriber list, sending the edit code to a confirmed subscriber along with an edit link for convenience. This should normally not be required since the code should be included in all other emails anyway, but people are people. Make sure to always include the necessary replacement tokens.', 'fcnen' ); ?></div>
+            <div class="fcnen-replacement-tokens"><?php _e( 'This email can be manually triggered in the subscriber list, sending the edit code to a confirmed subscriber along with an edit link for convenience. This should normally not be required since the code should be included in all other emails anyway, but people are people. Make sure to always include the necessary replacement tokens.', 'fcnen' ); ?></div>
             <div class="fcnen-spacer"></div>
             <div class="fcnen-left-right-wrap">
               <label for="fcnen-template-subject-code" class="offset-top"><?php _e( 'Subject', 'fcnen' ); ?></label>
@@ -786,7 +786,7 @@ function fcnen_settings_page() {
           </div>
 
           <div class="fcnen-box__row fcnen-box__vertical fcnen-template-wrapper hidden" id="layout-edit">
-            <div><?php _e( 'This email is sent whenever a subscriber updates their preferences, both as confirmation and security notification about the change. Just in case a malicious actor managed to acquire both their email address and code. Make sure to always include the necessary replacement tokens.', 'fcnen' ); ?></div>
+            <div class="fcnen-replacement-tokens"><?php _e( 'This email is sent whenever a subscriber updates their preferences, both as confirmation and security notification about the change. Just in case a malicious actor managed to acquire both their email address and code. Make sure to always include the necessary replacement tokens.', 'fcnen' ); ?></div>
             <div class="fcnen-spacer"></div>
             <div class="fcnen-left-right-wrap">
               <label for="fcnen-template-subject-edit" class="offset-top"><?php _e( 'Subject', 'fcnen' ); ?></label>
@@ -814,6 +814,7 @@ function fcnen_settings_page() {
           </div>
 
           <div class="fcnen-box__row fcnen-box__vertical fcnen-template-wrapper hidden" id="layout-notification">
+            <div class="fcnen-replacement-tokens"><?php _e( 'This email is sent for actual update notifications and consists of multiple parts: layout and loop partials. While this layout provides the surrounding body with some nice text of your choice (and the code, really important), the loop partials render the individual content matching the subscriber’s preferences. Make sure to always include the necessary replacement tokens.', 'fcnen' ); ?></div>
             <div class="fcnen-spacer"></div>
             <div class="fcnen-left-right-wrap">
               <label for="fcnen-template-subject-notification" class="offset-top"><?php _e( 'Subject', 'fcnen' ); ?></label>
@@ -830,6 +831,7 @@ function fcnen_settings_page() {
               <code>{{edit_link}}</code>
               <code>{{email}}</code>
               <code>{{code}}</code>
+              <code>{{updates}}</code>
             </div>
             <details class="fcnen-default-code">
               <summary><?php _e( 'Default HTML', 'fcnen' ); ?></summary>
