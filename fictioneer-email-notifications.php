fea8a5a797b336b613b63a45150792ca2ebff0a2
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2024-03-22 21:07:32 +0100

    Add meta database table

--- fictioneer-email-notifications.php
+++ fictioneer-email-notifications.php
@@ -267,6 +267,42 @@ function fcnen_create_notification_table() {
 }
 register_activation_hook( __FILE__, 'fcnen_create_notification_table' );
 
+/**
+ * Create the meta database table
+ *
+ * @since 0.1.0
+ * @global wpdb $wpdb  The WordPress database object.
+ */
+
+function fcnen_create_meta_table() {
+  global $wpdb;
+
+  if ( ! function_exists( 'dbDelta' ) ) {
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+  }
+
+  // Setup
+  $table_name = $wpdb->prefix . 'fcnen_meta';
+  $charset_collate = $wpdb->get_charset_collate();
+
+  // Skip if the table already exists
+  if ( $wpdb->get_var( "SHOW TABLES LIKE '$table_name'" ) == $table_name ) {
+    return;
+  }
+
+  // Table creation query
+  $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    post_id BIGINT UNSIGNED NOT NULL,
+    meta LONGTEXT NOT NULL,
+    PRIMARY KEY (id),
+    INDEX post_id_index (post_id)
+  ) $charset_collate;";
+
+  dbDelta( $sql );
+}
+register_activation_hook( __FILE__, 'fcnen_create_meta_table' );
+
 // =======================================================================================
 // CRON JOBS
 // =======================================================================================
